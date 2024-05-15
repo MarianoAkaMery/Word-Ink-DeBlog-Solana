e6da07bd283e47d7ffdb141f89e07d3abc505d29
Mariano Salvatore Librici <42919644+MarianoAkaMery@users.noreply.github.com>
2024-05-15 10:23:21 +0200

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const BLOG_PROGRAM_KEY = new PublicKey("3dh1vYL5M1c8fVCeUoKrsRhzyKaac8ZsHKYPS3Myghze")
+export const BLOG_PROGRAM_KEY = new PublicKey("AJ1d6UkWZ7aNAnYXE9PCQp7XhEceJbbsWCQsUbiD3JV6")
